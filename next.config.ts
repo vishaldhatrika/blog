b73ece3b5b65e5cd53918f814935f5ab4d5cb46e
Vishal Dhatrika <dvishal35@gmail.com>
2025-07-14 03:26:48 -0500

    image fix to allow from all sites

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,33 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "blogsmedia.lse.ac.uk",
-        pathname: "/blogs.dir/**", // Matches your image path
+        hostname: "**",         // Allow all domains
+        pathname: "**/*.jpg",   // Allow JPG
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "**/*.jpeg",  // Allow JPEG
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "**/*.png",   // Allow PNG
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "**/*.webp",  // Allow WebP
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "**/*.avif",  // Allow AVIF
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "**/*.svg",   // Allow SVG
       },
     ],
   },
