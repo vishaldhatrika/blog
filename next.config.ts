0e80b3b90e93364d4c1f2dc6344d3122b10a175b
Vishal Dhatrika <dvishal35@gmail.com>
2025-07-14 03:20:55 -0500

    image fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blogsmedia.lse.ac.uk",
+        pathname: "/blogs.dir/**", // Matches your image path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
